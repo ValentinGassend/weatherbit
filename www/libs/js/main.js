1c2408dec140bef02ca37524194ae0cd6327236b
ValentinGassend <valentin.gassend@gmail.com>
2022-01-03 14:32:53 +0100

    Searchbar animation done

--- www/libs/js/main.js
+++ www/libs/js/main.js
@@ -1,4 +1,14 @@
 function search() {
-    //document.getElementById("postclick-search").opacity.value = 1;
-    console.log(document.getElementById("postclick-search").style.opacity = 1);
+    document.getElementById("postclick-search").opacity = "1";
+    document.getElementById("form").style.bottom = "50vh";
+    document.getElementById("blur").style.display = "block";
+    //console.log(document.getElementById("form").bottom);
+    //document.getElementById("postclick-search").opacity = "1";
+
 }
+var clickHandler = function () {
+    document.getElementById("blur").style.display = "none";
+    document.getElementById("form").style.bottom = "-100px";
+}
+
+document.getElementById('blur').addEventListener('click', clickHandler);
